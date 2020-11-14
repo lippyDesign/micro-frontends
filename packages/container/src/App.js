8acd46d51a82972589c30783b3ac22a48e18221d
Vladimir <volo_lipu@yahoo.com>
2020-11-14 10:14:45 -0800

    fixed heart

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from './components/MarketingApp';
 
 export default () => {
   return <div>
-    <h1>I <3 Hrushka!!!!!</h1>
+    <h1>I love Hrushka!!!!!</h1>
     <hr />
     <MarketingApp />
   </div>
