fb4dabfb9716540b35c44c9d1aed01a782410ba2
Vladimir <volo_lipu@yahoo.com>
2020-11-14 10:25:41 -0800

    blah

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from './components/MarketingApp';
 
 export default () => {
   return <div>
-    <h1>I love Hrushka!!!!!</h1>
+    <h1>I love Hrushka!!!!! Meow</h1>
     <hr />
     <MarketingApp />
   </div>
