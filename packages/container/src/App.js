cc9aafb078485478f2a9f625f45cb11f227af6eb
Vladimir <volo_lipu@yahoo.com>
2020-11-14 21:15:56 -0800

    added dashboard

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,21 +1,31 @@
-import React, { lazy, Suspense, useState } from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import React, { lazy, Suspense, useState, useEffect } from 'react';
+import { Switch, Route, BrowserRouter, Router, Redirect } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+import { createBrowserHistory } from 'history';
 
 import Header from './components/Header';
 import Progress from './components/Progress';
 
 const AuthLazy = lazy(() => import('./components/AuthApp'));
 const MarketingLazy = lazy(() => import('./components/MarketingApp'));
+const DashboardLazy = lazy(() => import('./components/DashboardApp'));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co',
 });
 
+const history = createBrowserHistory();
+
 export default () => {
   const [isSignedIn, setIsSignedIn] = useState(false);
 
-  return <BrowserRouter>
+  useEffect(() => {
+    if (isSignedIn) {
+      history.push('/dashboard');
+    }
+  }, [isSignedIn]);
+
+  return <Router history={history}>
     <StylesProvider generateClassName={generateClassName}>
       <div>
         <Header isSignedIn={isSignedIn} onSignOut={() => setIsSignedIn(false)} />
@@ -24,10 +34,14 @@ export default () => {
             <Route path="/auth">
               <AuthLazy onSignIn={() => setIsSignedIn(true)} />
             </Route>
+            <Route path="/dashboard">
+              {!isSignedIn && <Redirect to="/" />}
+              <DashboardLazy />
+            </Route>
             <Route path="/" component={MarketingLazy} />
           </Switch>
         </Suspense>
       </div>
     </StylesProvider>
-  </BrowserRouter>
+  </Router>
 };
--- packages/dashboard/config/webpack.dev.js
+++ packages/dashboard/config/webpack.dev.js
@@ -1,4 +1,5 @@
 const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const packageJson = require('../package.json');
 
@@ -7,21 +8,26 @@ const commonConfig = require('./webpack.common');
 const devConfig = {
   mode: 'development',
   output: {
-    publicPath: 'http://localhost:8080/', // needed because subroutes
+    publicPath: 'http://localhost:8083/', // needed because subroutes
   },
   devServer: {
-    port: 8080,
+    port: 8083,
     historyApiFallback: {
       index: 'index.html',
     },
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    }
   },
   plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
     new ModuleFederationPlugin({
-      name: 'container',
-      remotes: {
-        marketing: 'marketing@http://localhost:8081/remoteEntry.js',
-        auth: 'auth@http://localhost:8082/remoteEntry.js',
-        dashboard: 'dashboard@http://localhost:8083/remoteEntry.js'
+      name: 'dashboard',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './DashboardApp': './src/bootstrap',
       },
       shared: packageJson.dependencies
     }),
